e04b6ba65810f975cd3abeba99ffc4c82086e19f
Teppo Kurki <teppo.kurki@iki.fi>
2014-04-30 15:47:14 +0300

    Rename

--- test/127245_rudder.js
+++ test/127245_rudder.js
@@ -3,15 +3,15 @@ chai.Should();
 
 describe('127245_rudder ', function () {
   it('complete sentence converts', function () {
-    var msg = require("../n2kMapper.js").toNested(
+    var msgs = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2013-10-08-15:47:28.263","prio":"2","src":"204","dst":"255","pgn":"127245","description":"Rudder","fields":{"Instance":"0","Position":"-0.7"}}'));
-    msg[0].vessel.rudder.should.have.property('value', -0.7);
-    msg.length.should.equal(1);
+    msgs[0].vessel.rudder.should.have.property('value', -0.7);
+    msgs.length.should.equal(1);
   });
 
   it('direction order is not handled', function () {
-    var msg = require("../n2kMapper.js").toNested(
+    var msgs = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2013-10-08-15:47:28.263","prio":"2","src":"204","dst":"255","pgn":"127245","description":"Rudder","fields":{"Instance":"252","Direction Order":"0"}}'));
-    msg.length.should.equal(0);
+    msgs.length.should.equal(0);
   });
 });
