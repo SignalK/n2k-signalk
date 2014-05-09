d60556dbb21e2e3efc5873c15a338b5629c7009c
Teppo Kurki <teppo.kurki@iki.fi>
2014-05-09 18:11:09 +0300

    Fix message

--- test/128275_log.js
+++ test/128275_log.js
@@ -3,7 +3,7 @@ chai.Should();
 chai.use(require('chai-things'));
 
 
-describe('128267_water_depth', function () {
+describe('128275 log', function () {
   it('complete sentence converts', function () {
     var msgs = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2013-10-08-16:04:06.060","prio":"6","src":"1","dst":"255","pgn":"128275","description":"Distance Log","fields":{"Log":"2229808","Trip Log":"4074"}}'));
