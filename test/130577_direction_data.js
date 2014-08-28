725399b29b12f6a4fbfcc486c70f091c1b6f1fac
Teppo Kurki <teppo.kurki@iki.fi>
2014-08-28 09:16:07 +0300

    Remove console.logs

--- test/130577_direction_data.js
+++ test/130577_direction_data.js
@@ -7,7 +7,6 @@ describe('130577 direction_data', function () {
   it('sentence without drift converts', function () {
     var msgs = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2014-08-15-10:01:35.236","prio":"3","src":"160","dst":"255","pgn":"130577","description":"Direction Data","fields":{"Data Mode":"Autonomous","COG Reference":"True","SID":"21","COG":"70.1","SOG":"0.01"}}'));
-    console.log(msgs);
     msgs.length.should.equal(2);
     msgs.should.contain.an.item.with.deep.property('navigation.speedOverGround');
     msgs.should.contain.an.item.with.deep.property('navigation.speedOverGround.value', 0.01);
@@ -17,7 +16,6 @@ describe('130577 direction_data', function () {
   it('sentence with drift converts', function () {
     var msgs = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2014-08-15-18:00:00.755","prio":"3","src":"160","dst":"255","pgn":"130577","description":"Direction Data","fields":{"Data Mode":"Autonomous","COG Reference":"True","SID":"84","COG":"206.9","SOG":"3.51","Set":"58.9","Drift":"0.28"}}'));
-    console.log(msgs);
     msgs.length.should.equal(4);
     msgs.should.contain.an.item.with.deep.property('navigation.speedOverGround');
     msgs.should.contain.an.item.with.deep.property('navigation.speedOverGround.value', 3.51);
