6647825057730d47813769d80ab07097ab17c59f
Teppo Kurki <teppo.kurki@iki.fi>
2014-08-29 18:11:10 +0300

    Remove console.log

--- test/129291_set_drift_rapid_update.js
+++ test/129291_set_drift_rapid_update.js
@@ -7,7 +7,6 @@ describe('129291 set & drift rapid update', function () {
   it('complete sentence converts', function () {
     var msgs = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2014-08-15-18:00:06.573","prio":"3","src":"160","dst":"255","pgn":"129291","description":"Set & Drift, Rapid Update","fields":{"Set Reference":"True","Set":"212.6","Drift":"0.24"}}'));
-    console.log(msgs);
     msgs.length.should.equal(2);
     msgs.should.contain.an.item.with.deep.property('navigation.setTrue');
     msgs.should.contain.an.item.with.deep.property('navigation.setTrue.value', 212.6);
