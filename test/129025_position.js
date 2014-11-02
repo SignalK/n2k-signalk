7e4c6a302e0452a32db5c50c750665c512259db6
Teppo Kurki <teppo.kurki@iki.fi>
2014-11-02 18:45:45 +0200

    Remove console.log

--- test/129025_position.js
+++ test/129025_position.js
@@ -6,7 +6,6 @@ describe('129025 Position, rapid update ', function () {
   it('complete sentence converts', function () {
     var tree = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2013-10-08-15:47:28.264","prio":"2","src":"2","dst":"255","pgn":"129025","description":"Position, Rapid Update","fields":{"Latitude":"60.1445540","Longitude":"24.7921348"}}'));
-    console.log(JSON.stringify(tree, undefined, 2));
     tree.navigation.position.longitude.should.equal(24.7921348);
     tree.navigation.position.latitude.should.equal(60.144554);
     tree.should.be.validSignalK;
