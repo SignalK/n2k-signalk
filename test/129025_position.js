d1454082315cbfdbb746f9d5e7ae698dcea5730d
Teppo Kurki <teppo.kurki@iki.fi>
2015-04-23 14:17:09 +0300

    Move timestamp to the correct place in delta and add first delta validation test

--- test/129025_position.js
+++ test/129025_position.js
@@ -2,13 +2,18 @@ var chai = require("chai");
 chai.Should();
 chai.use(require('signalk-schema').chaiModule);
 
+var msg = JSON.parse('{"timestamp":"2013-10-08-15:47:28.264","prio":"2","src":"2","dst":"255","pgn":"129025","description":"Position, Rapid Update","fields":{"Latitude":"60.1445540","Longitude":"24.7921348"}}');
+
 describe('129025 Position, rapid update ', function () {
-  it('complete sentence converts', function () {
-    var tree = require("../n2kMapper.js").toNested(
-      JSON.parse('{"timestamp":"2013-10-08-15:47:28.264","prio":"2","src":"2","dst":"255","pgn":"129025","description":"Position, Rapid Update","fields":{"Latitude":"60.1445540","Longitude":"24.7921348"}}'));
+  it('complete sentence converts to tree', function () {
+    var tree = require("../n2kMapper.js").toNested(msg);
     tree.navigation.position.longitude.should.equal(24.7921348);
     tree.navigation.position.latitude.should.equal(60.144554);
     tree.should.be.validSignalK;
   });
 
+  it('complete sentence produces valid delta', function () {
+    var delta = require("../n2kMapper.js").toDelta(msg);
+    delta.should.be.validSignalKDelta;
+  });
 });
