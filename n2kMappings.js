4f08905cd1659edf67989eadfbf9de1e2ad30268
Teppo Kurki <tjk@iki.fi>
2015-07-31 20:05:27 +0300

    Push attitude fields under navigation.attitude See https://github.com/SignalK/specification/issues/45

--- n2kMappings.js
+++ n2kMappings.js
@@ -95,16 +95,14 @@ exports.mappings =
   //Attitude
   '127257': [
     {
-      source: 'Yaw',
-      node: 'navigation.yaw'
-    },
-    {
-      source: 'Pitch',
-      node: 'navigation.pitch'
-    },
-    {
-      source: 'Roll',
-      node: 'navigation.roll'
+      value: function (n2k) {
+        return {
+          yaw: Number(n2k.fields.Yaw),
+          pitch: Number(n2k.fields.Pitch),
+          roll: Number(n2k.fields.Roll)
+        }
+      },
+      node: 'navigation.attitude'
     }
   ],
   //Wind data
