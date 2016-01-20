899ff8595dd90c558701f5cf84f1a549f1745793
Damian Hamill <damianham@gmail.com>
2016-01-20 14:34:32 +0000

    rename alternatorPotential to alternatorVoltage

--- test/127489_engine_params.js
+++ test/127489_engine_params.js
@@ -11,8 +11,8 @@ describe('127489 engine parameters Port', function () {
       JSON.parse('{"timestamp":"2015-01-15-16:25:14.952","prio":"2","src":"17","dst":"255","pgn":"127489",	"description":"Engine Parameters, Dynamic","fields":{"Engine Instance":"Single Engine or Dual Engine Port","Temperature":"29.85","Alternator Potential":"12.60","Fuel Rate":"-0.1","Total Engine hours":"309960","Discrete Status 1":"0","Discrete Status 2":"0","Percent Engine Load":"-1","Percent Engine Torque":"-1"}}'));
     tree.should.have.deep.property('propulsion.port.temperature');
     tree.should.have.deep.property('propulsion.port.temperature.value', 29.85);
-    tree.should.have.deep.property('propulsion.port.alternatorPotential');
-    tree.should.have.deep.property('propulsion.port.alternatorPotential.value', 12.60);
+    tree.should.have.deep.property('propulsion.port.alternatorVoltage');
+    tree.should.have.deep.property('propulsion.port.alternatorVoltage.value', 12.60);
     tree.should.have.deep.property('propulsion.port.fuelRate');
     tree.should.have.deep.property('propulsion.port.fuelRate.value', -0.1); 
     tree.should.be.validSignalKVesselIgnoringIdentity;
@@ -25,8 +25,8 @@ describe('127489 engine parameters Starboard', function () {
       JSON.parse('{"timestamp":"2015-01-15-16:25:14.952","prio":"2","src":"17","dst":"255","pgn":"127489","description":"Engine Parameters, Dynamic","fields":{"Engine Instance":"Dual Engine Starboard","Temperature":"29.85","Alternator Potential":"12.60","Fuel Rate":"-0.1","Total Engine hours":"309960","Discrete Status 1":"0","Discrete Status 2":"0","Percent Engine Load":"-1","Percent Engine Torque":"-1"}}'));
     tree.should.have.deep.property('propulsion.starboard.temperature');
     tree.should.have.deep.property('propulsion.starboard.temperature.value', 29.85);
-    tree.should.have.deep.property('propulsion.starboard.alternatorPotential');
-    tree.should.have.deep.property('propulsion.starboard.alternatorPotential.value', 12.60);
+    tree.should.have.deep.property('propulsion.starboard.alternatorVoltage');
+    tree.should.have.deep.property('propulsion.starboard.alternatorVoltage.value', 12.60);
     tree.should.have.deep.property('propulsion.starboard.fuelRate');
     tree.should.have.deep.property('propulsion.starboard.fuelRate.value', -0.1); 
     tree.should.be.validSignalKVesselIgnoringIdentity;
