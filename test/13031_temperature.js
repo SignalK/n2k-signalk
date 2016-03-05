efaa093a8fe7cebea935c6ca71b516e5da2545b5
Teppo Kurki <teppo.kurki@iki.fi>
2016-03-05 16:24:40 +0200

    TEMPORARILY add test as toString

--- test/13031_temperature.js
+++ test/13031_temperature.js
@@ -3,10 +3,12 @@ chai.Should();
 chai.use(require('chai-things'));
 chai.use(require('signalk-schema').chaiModule);
 var debug = require('debug')('n2k-signalk:test:130312');
+var _ = require('lodash');
 
 
 var msgs = require('./130310-2.json');
 
+/*
 describe('130312 with Inside Temperature', function() {
   console.log(JSON.stringify(msgs['130312-insidetemp'], null, 2));
 
@@ -19,4 +21,16 @@ describe('130312 with Inside Temperature', function() {
   it('is valid SignalK', function() {
     tree.should.be.validSignalKVesselIgnoringIdentity;
   });
+});
+*/
+
+
+describe('temps', function() {
+  it("works", function() {
+    var n2kMapper = require("../n2kMapper.js");
+    _.forOwn(msgs, function(msg, key) {
+      console.log(key);
+      console.log(n2kMapper.toDelta(msg).updates[0].values);
+    })
+  })
 });
