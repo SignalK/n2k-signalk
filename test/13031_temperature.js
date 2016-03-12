c76c6e94dee2348f40c5eda49025607af55afe80
Teppo Kurki <teppo.kurki@iki.fi>
2016-03-12 20:12:31 +0200

    Verify paths by mapping to full doc, check all mappings

--- test/13031_temperature.js
+++ test/13031_temperature.js
@@ -25,21 +25,48 @@ describe('130312 with Inside Temperature', function() {
 */
 
 
-describe('temps', function() {
-  it("works", function() {
+describe('Temperature: ', function() {
+  it("examples work", function() {
     var n2kMapper = require("../n2kMapper.js");
-    var full = new (require('signalk-schema').FullSignalK)();
+    var full = new(require('signalk-schema').FullSignalK)();
     _.forOwn(msgs, function(msg, key) {
-//      console.log(key);
       var delta = n2kMapper.toDelta(msg);
-//      console.log(JSON.stringify(delta, null, 2));
       delta.context = 'vessels.urn:mrn:imo:mmsi:230099999';
       full.addDelta(delta);
-      delta.should.be.validSignalKDelta
-      delta.updates[0].values.forEach(function(pathValue){
-//        console.log(" " + pathValue.path + ":" + pathValue.value)
+      delta.should.be.validSignalKDelta;
+      delta.updates[0].values.forEach(function(pathValue) {
       })
     })
-//    console.log(JSON.stringify(full.retrieve(), null, 2));
+    var fullDoc = full.retrieve();
+    fullDoc.vessels['urn:mrn:imo:mmsi:230099999'].mmsi = '230099999';
+    fullDoc.should.be.validSignalK;
+  })
+
+  it("all 130312 mappings are valid", function() {
+    var temperatureMappings = require("../n2kMappings").temperatureMappings;
+    var full = new(require('signalk-schema').FullSignalK)();
+    _.forOwn(temperatureMappings, function(mapping, key) {
+      var delta = {
+        context: 'vessels.urn:mrn:imo:mmsi:230099999',
+        updates: [{
+          source: {
+            "label": "",
+            "type": "NMEA2000",
+            "pgn": 130312,
+            "src": "36",
+            "instance": "0"
+          },
+          timestamp: '',
+          values: [{
+            path: mapping.path,
+            value: 0
+          }]
+        }]
+      }
+      full.addDelta(delta);
+    })
+    var fullDoc = full.retrieve();
+    fullDoc.vessels['urn:mrn:imo:mmsi:230099999'].mmsi = '230099999';
+    fullDoc.should.be.validSignalK;
   })
 });
