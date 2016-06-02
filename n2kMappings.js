e981e1bca8756ad9e81331ed90157f8ac117b5d7
Teppo Kurki <teppo.kurki@iki.fi>
2016-06-02 08:35:04 +0300

    Add 129284 Navigation Data

--- n2kMappings.js
+++ n2kMappings.js
@@ -1,3 +1,12 @@
+var CALCULATION_TYPE = "Calculation Type";
+var RHUMBLINE = "Rhumb Line";
+var GREATCIRCLE = "Great Circle";
+
+function calculationType(n2k) {
+  console.log(n2k)
+  return n2k.fields[CALCULATION_TYPE] === GREATCIRCLE ? 'GreatCircle' : 'Rhumbline';
+}
+
 function getMmsiContext(n2k) {
   return 'vessels.urn:mrn:imo:mmsi:' + n2k.fields['User ID'];
 }
@@ -234,6 +243,26 @@ exports.mappings = {
       }
     }
   }],
+
+  //Navigation data
+  '129284': [{
+    node: function(n2k) {return 'navigation.course' + calculationType(n2k) +  '.bearingTrackTrue'},
+    source: 'Bearing, Origin to Destination Waypoint'
+  }, {
+    node: function(n2k) {return 'navigation.course' + calculationType(n2k) + '.nextPoint'},
+    value: function(n2k) {
+      return  {
+        velocityMadeGood: Number(n2k.fields['Waypoint Closing Velocity']),
+        distance: Number(n2k.fields['Distance to Waypoint']),
+        position: {
+          longitude: Number(n2k.fields['Destination Longitude']),
+          latitude: Number(n2k.fields['Destination Latitude'])
+        },
+        'bearingTrue': n2k.fields['Bearing, Position to Destination Waypoint']
+      };
+    }
+  }],
+
 
   //Set & Drift rapid update
   '129291': [{
