4aa66d67e7ecd8feb33b3e1d1b47b07425a6d357
Scott Bender <scott@scottbender.net>
2016-08-27 09:28:21 -0400

    Added 127505 Tanks

--- n2kMappings.js
+++ n2kMappings.js
@@ -392,8 +392,29 @@ exports.mappings = {
       return n2k.fields["Temperature Instance"] + '';
     },
     source: 'Actual Temperature'
-  }]
+    }],
 
+  //Tanks
+  '127505': [{
+      source: 'Level',
+      node: function(n2k) { 
+	return 'tanks.' + n2k.fields['Type'] + '_' + n2k.fields['Instance'] + '.currentLevel;' 
+      }
+    },
+    {
+      source: 'Capacity',
+      node: function(n2k) { 
+	return 'tanks.' + n2k.fields['Type'] + '_' + n2k.fields['Instance'] + '.capacity'; 
+      },
+    },
+    {
+      node: function(n2k) {
+	return 'tanks.' + n2k.fields['Type'] + '_' + n2k.fields['Instance'] + '.name'; 
+      },
+      value: function(n2k) { 
+	return n2k.fields['Type'] + ' ' + n2k.fields['Instance']; 
+      }
+    }],
 }
 
 /*
---
+++ test/127505_tanks.js
@@ -0,0 +1,19 @@
+var chai = require("chai");
+chai.Should();
+chai.use(require('chai-things'));
+chai.use(require('signalk-schema').chaiModule);
+
+
+describe('127505 tanks', function () {
+  it('complete sentence converts', function () {
+    var tree = require("../n2kMapper.js").toNested(
+      JSON.parse('{"timestamp":"2016-08-22T16:02:55.773Z","prio":6,"src":17,"dst":255,"pgn":127505,"description":"Fluid Level","fields":{"Instance":0,"Type":"Fuel","Level":30.280,"Capacity":49.0}}'));
+    tree.should.have.deep.property('tanks.Fuel_0.currentLevel', 30.280);
+    tree.should.have.deep.property('tanks.Fuel_0.capacity', 49.0);
+    tree.should.have.deep.property('tanks.Fuel_0.name', 'Fuel 0');
+    tree.should.be.validSignalKVesselIgnoringIdentity;
+  });
+});
+
+
+
