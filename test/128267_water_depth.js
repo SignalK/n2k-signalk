f13b4494ec350a942e6e7df2e6d88a7902cc11bf
Scott Bender <scott@scottbender.net>
2016-08-27 11:50:10 -0400

    Fixed syntax error in JSON

--- test/128267_water_depth.js
+++ test/128267_water_depth.js
@@ -8,7 +8,7 @@ chai.use(require('signalk-schema').chaiModule);
 describe('128267_water_depth', function () {
   it('complete sentence converts', function () {
     var tree = require("../n2kMapper.js").toNested(
-      JSON.parse('{"timestamp":"2013-10-08-15:47:28.280","prio":"3","src":"1","dst":"255","pgn":"128267","description":"Water Depth","fields":{"SID":"91","Depth":"8.20" "Offset":0.304}}'));
+      JSON.parse('{"timestamp":"2013-10-08-15:47:28.280","prio":"3","src":"1","dst":"255","pgn":"128267","description":"Water Depth","fields":{"SID":"91","Depth":"8.20", "Offset":0.304}}'));
     tree.should.have.deep.property('environment.depth.belowTransducer');
     tree.should.have.deep.property('environment.depth.belowTransducer.value', 8.20);
     tree.should.have.deep.property('environment.depth.surfaceToTransducer');
