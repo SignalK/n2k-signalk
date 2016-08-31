0d6dedd97e01698d079fc6f8087c78e93d00b1b6
Scott Bender <scott@scottbender.net>
2016-08-31 17:23:58 -0400

    Fixed to provide correct format

--- n2kMappings.js
+++ n2kMappings.js
@@ -396,14 +396,10 @@ exports.mappings = {
 
   //Battery Voltage
   '127508': [{
+    source: 'Voltage',
     node: function(n2k) {
-	return 'electrical.batteries.' + n2k.fields['Battery Instance']
+	return 'electrical.batteries.' + n2k.fields['Battery Instance'] + '.voltage'
     },
-    value: function(n2k) {
-	return { 
-	  voltage: n2k.fields['Voltage']
-	}
-      }
   }],
 }
 
--- test/127508_battery_voltage.js
+++ test/127508_battery_voltage.js
@@ -7,7 +7,7 @@ describe('127508 battery voltage', function () {
   it('complete sentence converts', function () {
     var tree = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2016-08-22T16:02:55.272Z","prio":6,"src":17,"dst":255,"pgn":127508,"description":"Battery Status","fields":{"Battery Instance":1,"Voltage":13.11,"SID":0}}'));
-    tree.should.have.deep.property('electrical.batteries.1.voltage', 13.11);
+    tree.should.have.deep.property('electrical.batteries.1.voltage.value', 13.11);
     tree.should.be.validSignalKVesselIgnoringIdentity;
   });
 });
