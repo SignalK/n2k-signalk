286e6703c7d468c977934c5c287f21772480c1f8
Scott Bender <scott@scottbender.net>
2016-09-01 12:50:52 -0400

    Add 127508 Battery Voltage

---
+++ test/127508_battery_voltage.js
@@ -0,0 +1,13 @@
+var chai = require("chai");
+chai.Should();
+chai.use(require('chai-things'));
+
+
+describe('127508 battery voltage', function () {
+  it('complete sentence converts', function () {
+    var tree = require("../n2kMapper.js").toNested(
+      JSON.parse('{"timestamp":"2016-08-22T16:02:55.272Z","prio":6,"src":17,"dst":255,"pgn":127508,"description":"Battery Status","fields":{"Battery Instance":1,"Voltage":13.11,"SID":0}}'));
+    tree.should.have.deep.property('electrical.batteries.1.voltage.value', 13.11);
+    tree.should.be.validSignalKVesselIgnoringIdentity;
+  });
+});
