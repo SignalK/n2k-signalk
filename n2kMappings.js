f00301dbb407aa43460b0fb20e31a434bcc4f3b2
Scott Bender <scott@scottbender.net>
2016-10-19 11:41:30 -0400

    Convert RPM's to Hz per the spec (#34)

--- n2kMappings.js
+++ n2kMappings.js
@@ -103,17 +103,23 @@ exports.mappings = {
   }],
   // Engine speed data
   '127488': [{
-    source: 'Engine Speed',
     node: 'propulsion.port.revolutions',
     filter: function(n2k) {
       return n2k.fields['Engine Instance'] === 'Single Engine or Dual Engine Port';
-    }
+    },
+    value: function(n2k) {
+      var rpm = Number(n2k.fields['Engine Speed'])
+      return rpm / 60.0;
+    }      
   }, {
-    source: 'Engine Speed',
     node: 'propulsion.starboard.revolutions',
     filter: function(n2k) {
       return n2k.fields['Engine Instance'] === 'Dual Engine Starboard';
-    }
+    },
+    value: function(n2k) {
+      var rpm = Number(n2k.fields['Engine Speed'])
+      return rpm / 60.0;
+    }      
   }],
   // Engine operating parameters
   '127489': [{
