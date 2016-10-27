4c2e596feb5b9a65d39e9f19e0ce2102ab5233ac
Scott Bender <scott@scottbender.net>
2016-10-27 17:31:23 -0400

    Added 65288 Seatalk: Alarm

--- n2kMappings.js
+++ n2kMappings.js
@@ -444,6 +444,35 @@ exports.mappings = {
       return 'electrical.batteries.' + n2k.fields['Battery Instance'] + '.voltage'
     },
   }],
+
+  // Seatalk: Alarm
+  '65288': [{
+    node: function(n2k) {
+      var alarmName = n2k.fields['Alarm Group'].toLowerCase().replace(/ /g, '') + n2k.fields['Alarm ID'].replace(/ /g, '');
+      return 'notifications.' + alarmName;
+    },
+    value: function(n2k) {
+      var state = n2k.fields['Alarm Status'];
+      
+      var method = [ 'visual' ];
+      
+      if ( state == 'Alarm condition met and not silenced' )
+	method.push('sound');
+      
+      if ( state == 'Alarm condition not met' )
+	state = 'normal'
+      else
+	state = 'alarm'
+      
+      
+      return {
+	message: n2k.fields['Alarm ID'],
+	method: method,
+	state: state,
+	timestamp: n2k.timestamp
+      }
+    }
+  }]
 }
 
 /*
