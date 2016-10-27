7d9b2f11c2a75fde88260f42e100418a8fac7e63
Scott Bender <scott@scottbender.net>
2016-10-27 17:17:59 -0400

    Added 65379 Seatalk: Pilot Mode

--- n2kMappings.js
+++ n2kMappings.js
@@ -444,6 +444,25 @@ exports.mappings = {
       return 'electrical.batteries.' + n2k.fields['Battery Instance'] + '.voltage'
     },
   }],
+
+  //Seatalk: Pilot Mode
+  '65379': [{
+    node: 'steering.autopilot.state',
+    value: function(n2k) {
+      var mode = Number(n2k.fields['Pilot Mode']);
+      var subMode = Number(n2k.fields['Sub Mode']);
+      if ( mode == 0 && subMode == 0 )
+	return 'standby';
+      else if ( mode == 0 && subMode == 1 )
+	return 'wind';
+      else if ( (mode == 128 || mode == 129) && subMode == 1 )
+	return 'route';
+      else if ( mode == 64 && subMode == 0 )
+	return 'auto';
+      else
+	return 'standby';
+    }
+  }],
 }
 
 /*
