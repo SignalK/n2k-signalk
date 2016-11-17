d30e05df9dcf7226bcae36a4cac8c4b57cc13f41
Scott Bender <scott@scottbender.net>
2016-11-17 12:36:42 -0500

    Fixed failing tests

--- test/65288_seatalk_alarm.js
+++ test/65288_seatalk_alarm.js
@@ -8,8 +8,8 @@ describe('65288 Seatalk Alarm', function () {
   it('complete alarm sentence converts waypoint advance alarm with sound', function () {
     var tree = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2016-10-18T15:52:48.152Z","prio":7,"src":204,"dst":255,"pgn":65288,"description":"Seatalk: Alarm","fields":{"Manufacturer Code":"Raymarine","Industry Code":"Marine Industry","Alarm Status":"Alarm condition met and not silenced","Alarm ID":"Pilot Way Point Advance","Alarm Group":"Autopilot","Alarm Priority":"0"}}'));
-    tree.should.have.deep.property('notifications.autopilotPilotWayPointAdvance.state', 'alarm');
-    tree.notifications.autopilotPilotWayPointAdvance.method.should.eql([ 'visual', 'sound']);
+    tree.should.have.deep.property('notifications.autopilot.PilotWayPointAdvance.state', 'alarm');
+    tree.notifications.autopilot.PilotWayPointAdvance.method.should.eql([ 'visual', 'sound']);
     //https://github.com/SignalK/specification/issues/281
     //tree.should.be.validSignalKVesselIgnoringIdentity;
   });
@@ -17,7 +17,7 @@ describe('65288 Seatalk Alarm', function () {
   it('complete alarm sentence converts waypoint advance normal', function () {
     var tree = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2016-10-18T15:52:49.063Z","prio":7,"src":204,"dst":255,"pgn":65288,"description":"Seatalk: Alarm","fields":{"Manufacturer Code":"Raymarine","Industry Code":"Marine Industry","Alarm Status":"Alarm condition not met","Alarm ID":"Pilot Way Point Advance","Alarm Group":"Autopilot","Alarm Priority":"0"}}'));
-    tree.should.have.deep.property('notifications.autopilotPilotWayPointAdvance.state', 'normal');
+    tree.should.have.deep.property('notifications.autopilot.PilotWayPointAdvance.state', 'normal');
     //https://github.com/SignalK/specification/issues/281
     //tree.should.be.validSignalKVesselIgnoringIdentity;
   });
@@ -25,8 +25,8 @@ describe('65288 Seatalk Alarm', function () {
   it('complete alarm sentence converts waypoint advance alarm silenced', function () {
     var tree = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2016-10-18T15:54:16.750Z","prio":7,"src":204,"dst":255,"pgn":65288,"description":"Seatalk: Alarm","fields":{"Manufacturer Code":"Raymarine","Industry Code":"Marine Industry","Alarm Status":"Alarm condition met and silenced","Alarm ID":"Pilot Way Point Advance","Alarm Group":"Autopilot","Alarm Priority":"0"}}'));
-    tree.should.have.deep.property('notifications.autopilotPilotWayPointAdvance.state', 'alarm');
-    tree.notifications.autopilotPilotWayPointAdvance.method.should.eql([ 'visual' ]);
+    tree.should.have.deep.property('notifications.autopilot.PilotWayPointAdvance.state', 'alarm');
+    tree.notifications.autopilot.PilotWayPointAdvance.method.should.eql([ 'visual' ]);
     //https://github.com/SignalK/specification/issues/281
     //tree.should.be.validSignalKVesselIgnoringIdentity;
   });
