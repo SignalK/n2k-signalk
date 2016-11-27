d69f72a967eddcdc006604b8d3cf9d98caa4de07
Scott Bender <scott@scottbender.net>
2016-11-27 11:34:05 -0500

    Handle negative transducer offsets correctly

--- test/128267_water_depth.js
+++ test/128267_water_depth.js
@@ -6,15 +6,20 @@ chai.use(require('signalk-schema').chaiModule);
 
 
 describe('128267_water_depth', function () {
-  it('complete sentence converts', function () {
+  it('complete positive offset sentence converts', function () {
     var tree = require("../n2kMapper.js").toNested(
       JSON.parse('{"timestamp":"2013-10-08-15:47:28.280","prio":"3","src":"1","dst":"255","pgn":"128267","description":"Water Depth","fields":{"SID":"91","Depth":"8.20", "Offset":0.304}}'));
-    tree.should.have.deep.property('environment.depth.belowTransducer');
     tree.should.have.deep.property('environment.depth.belowTransducer.value', 8.20);
-    tree.should.have.deep.property('environment.depth.surfaceToTransducer');
     tree.should.have.deep.property('environment.depth.surfaceToTransducer.value', 0.304);
-    tree.should.have.deep.property('environment.depth.belowSurface');
     tree.should.have.deep.property('environment.depth.belowSurface.value', 8.504);
+    tree.should.be.validSignalKVesselIgnoringIdentity;
+  });
+
+  it('complete negative offset sentence converts', function () {
+    var tree = require("../n2kMapper.js").toNested(
+      JSON.parse('{"timestamp":"2013-10-08-15:47:28.280","prio":"3","src":"1","dst":"255","pgn":"128267","description":"Water Depth","fields":{"SID":"91","Depth":"8.20", "Offset":-0.304}}'));
+    tree.should.have.deep.property('environment.depth.belowTransducer.value', 8.20);
+    tree.should.have.deep.property('environment.depth.transducerToKeel.value', -0.304);
     tree.should.be.validSignalKVesselIgnoringIdentity;
   });
 });
