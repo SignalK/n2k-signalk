b5d735ac0b41238b6aa68dacfc1223c293ab7201
Teppo Kurki <teppo.kurki@iki.fi>
2016-12-11 18:22:06 +0200

    Make sure Date and Time exist
    Apparently sometimes they do not.

--- pgns/126992.js
+++ pgns/126992.js
@@ -4,6 +4,9 @@ module.exports = [
     value: function(n2k) {
       return n2k.fields.Date.replace(/\./g, '-') + "T" + n2k.fields.Time + "Z";
     },
+    filter: function(n2k) {
+      return typeof n2k.fields['Date'] != 'undefined' && typeof n2k.fields['Time'] != 'undefined'
+    },
     node: 'navigation.datetime'
   }
 ]
