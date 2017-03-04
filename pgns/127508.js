9b509d71aa230c895db42bef38011301a999f64e
Scott Bender <scott@scottbender.net>
2017-03-04 17:21:51 -0500

    Added current for battery PGN 127508

--- pgns/127508.js
+++ pgns/127508.js
@@ -5,5 +5,10 @@ module.exports = [
     node: function(n2k) {
       return 'electrical.batteries.' + n2k.fields['Battery Instance'] + '.voltage'
     }
+  },{
+    source: 'Current',
+    node: function(n2k) {
+      return 'electrical.batteries.' + n2k.fields['Battery Instance'] + '.current'
+    }
   }
 ]
