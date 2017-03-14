7e55772524922650bdaa3e5f3bd3ab56a46f5839
Scott Bender <scott@scottbender.net>
2017-03-14 17:12:51 -0400

    Add missing data to pgn 129794
    Add design.draft.maximum, design.length.overall, design.aisShipType and design.beam

--- pgns/129794.js
+++ pgns/129794.js
@@ -8,7 +8,28 @@ module.exports = [
         name: n2k.fields.Name
       }
     }
-  }, {
+  },
+  {
+    node: 'navigation.destination.name',
+    source: 'Destination'
+  },
+  {
+    node: 'design.draft.maximum',
+    source: 'Draft'
+  },
+  {
+    node: 'design.length.overall',
+    source: 'Length'
+  },
+  {
+    node: 'design.aisShipType',
+    source: 'Type of ship'
+  },
+  {
+    node: 'design.beam',
+    source: 'Beam'
+  },
+  {
     context: getMmsiContext
   }
 ]
