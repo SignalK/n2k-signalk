fd468d6e2aa0322eb3ab97f9e9b786843077e22a
Teppo Kurki <teppo.kurki@iki.fi>
2017-03-19 18:27:12 +0200

    fix(schema conformance): nextPoint from object to individual fields
    https://github.com/SignalK/specification/pull/313

--- test/129284_navigation_data.js
+++ test/129284_navigation_data.js
@@ -11,9 +11,9 @@ describe('129284 Navigation Data', function () {
     var msg = JSON.parse('{"timestamp":"2016-05-03T18:19:05.588Z","prio":3,"src":4,"dst":255,"pgn":129284,"description":"Navigation Data","fields":{"SID":231,"Distance to Waypoint":1910.38,"Course/Bearing reference":"True","Perpendicular Crossed":"No","Arrival Circle Entered":"No","Calculation Type":"Great Circle", "ETA Time": "03:01:46","ETA Date":"2016.05.04","Bearing, Origin to Destination Waypoint":2.6435,"Bearing, Position to Destination Waypoint":2.7651,"Origin Waypoint Number":0,"Destination Waypoint Number":1,"Destination Latitude":60.1366607,"Destination Longitude":24.9068518,"Waypoint Closing Velocity":0.06}}');
     var tree = mapper.toNested(msg);
     tree.should.have.deep.property('navigation.courseGreatCircle.bearingTrackTrue.value', 2.6435);
-    tree.should.have.deep.property('navigation.courseGreatCircle.nextPoint.velocityMadeGood', 0.06);
-    tree.should.have.deep.property('navigation.courseGreatCircle.nextPoint.distance', 1910.38);
-    tree.should.have.deep.property('navigation.courseGreatCircle.nextPoint.bearingTrue', 2.7651);
+    tree.should.have.deep.property('navigation.courseGreatCircle.nextPoint.velocityMadeGood.value', 0.06);
+    tree.should.have.deep.property('navigation.courseGreatCircle.nextPoint.distance.value', 1910.38);
+    tree.should.have.deep.property('navigation.courseGreatCircle.nextPoint.bearingTrue.value', 2.7651);
     tree.should.have.deep.property('navigation.courseGreatCircle.nextPoint.position.latitude', 60.1366607);
     tree.should.have.deep.property('navigation.courseGreatCircle.nextPoint.position.longitude', 24.9068518);
     tree.should.be.validSignalKVesselIgnoringIdentity;
