63c8a200b1272615795b210ddc3f461cef2bfd93
Teppo Kurki <teppo.kurki@iki.fi>
2017-03-21 17:47:54 +0200

    fix: fix test to actually test something
    I am not quite sure if this is the most sane way to treat unknown PGNs, but
    as it has not caused noticeable problems and it is better to have a test
    against mistakenly changing this let's make the test do something
    meaningful.

--- test/unknown_pgn.js
+++ test/unknown_pgn.js
@@ -1,17 +1,10 @@
-var chai = require("chai");
+const chai = require("chai");
 chai.Should();
-
+const assert = require('assert')
 
 describe('Unknown pgn', function () {
-  it('return undefined', function () {
-    var tree = require("../n2kMapper.js").toDelta(
-      JSON.parse('{"timestamp":"2013-10-08-16:04:06.044","prio":"3","src":"1","dst":"255","pgn":"999999","description":"System Time","fields":{"SID":"222","Date":"2013.10.08", "Time": "16:04:00"}}'));
-
+  it('returns delta with one update that has no values', function () {
+    var delta = require("../n2kMapper.js").toDelta(JSON.parse('{"timestamp":"2013-10-08-16:04:06.044","prio":"3","src":"1","dst":"255","pgn":"999999","description":"System Time","fields":{"SID":"222","Date":"2013.10.08", "Time": "16:04:00"}}'));
+    assert.equal(delta.updates[0].values.length, 0)
   });
 });
-
-
-
-
-
-
