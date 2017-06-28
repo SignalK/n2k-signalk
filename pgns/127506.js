943156cb8dab1b9fa607c35dcce208a279ba5655
Scott Bender <scott@scottbender.net>
2017-06-28 13:50:47 -0400

    add: PGN 127506 DC Detailed Status

---
+++ pgns/127506.js
@@ -0,0 +1,24 @@
+
+module.exports = [
+  {
+    source: 'State of Charge',
+    node: function(n2k) {
+      return 'electrical.batteries.' + n2k.fields['DC Instance'] + '.capacity.stateOfCharge'
+    }
+  },{
+    source: 'State of Health',
+    node: function(n2k) {
+      return 'electrical.batteries.' + n2k.fields['DC Instance'] + '.capacity.stateOfHealth'
+    }
+  },{
+    source: 'Time Remaining',
+    node: function(n2k) {
+      return 'electrical.batteries.' + n2k.fields['DC Instance'] + '.capacity.timeRemaining'
+    }
+  },{
+    source: 'Ripple Voltage',
+    node: function(n2k) {
+      return 'electrical.batteries.' + n2k.fields['DC Instance'] + '.voltage.ripple'
+    }
+  }
+]
