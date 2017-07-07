6ebdea1a363d185c8b4da73df2ca85f390147d28
Scott Bender <scott@scottbender.net>
2017-07-07 17:19:19 -0400

    Add pgn 129283 cross track error

---
+++ pgns/129283.js
@@ -0,0 +1,22 @@
+// Cross Track Error
+
+module.exports = [
+  {
+    node: function (n2k, state) {
+      return (
+        'navigation.course' +
+        state.lastCourseCalculationType +
+        '.crossTrackError'
+      )
+    },
+    filter: function (n2k, state) {
+      return (
+        n2k.fields['Navigation Terminated'] &&
+        n2k.fields['Navigation Terminated'] === 'No' &&
+        typeof n2k.fields['XTE'] !== 'undefined' &&
+        typeof state.lastCourseCalculationType !== 'undefined'
+      )
+    },
+    source: 'XTE'
+  }
+]
