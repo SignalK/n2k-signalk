ff4a859ddad15dd79fa36d0309917c418ee7b812
Scott Bender <scott@scottbender.net>
2017-08-12 21:19:17 -0400

    add: PGN 127501 binary switch bank status

---
+++ pgns/127501.js
@@ -0,0 +1,24 @@
+module.exports = []
+
+for (var i = 1; i <= 28; i++) {
+  const idx = i
+  const field = 'Indicator' + idx
+  var mapping = {
+    node: function (n2k) {
+      return (
+        'electrical.switch.' +
+        n2k.fields['Indicator Bank Instance'] +
+        '.' +
+        idx +
+        '.state'
+      )
+    },
+    filter: function (n2k) {
+      return typeof n2k.fields[field] !== 'undefined'
+    },
+    value: function (n2k) {
+      return n2k.fields[field] == 'On'
+    }
+  }
+  module.exports.push(mapping)
+}
---
+++ test/127501_switch_bank_status.js
@@ -0,0 +1,21 @@
+var chai = require('chai')
+chai.Should()
+chai.use(require('chai-things'))
+
+describe('127501 switch bank status', function () {
+  it('complete sentence converts', function () {
+    var tree = require('../n2kMapper.js').toNested(
+      JSON.parse(
+        '{"timestamp":"2017-08-12T15:51:46.369Z","prio":3,"src":32,"dst":255,"pgn":127501,"description":"Binary Switch Bank Status","fields":{"Indicator Bank Instance":0,"Indicator1":"On","Indicator2":"Off","Indicator3":"On","Indicator4":"Off","Indicator5":"Off","Indicator6":"Off","Indicator7":"Off","list":[{}]}}'
+      )
+    )
+    tree.should.have.nested.property('electrical.switch.0.1.state.value', true)
+    tree.should.have.nested.property('electrical.switch.0.2.state.value', false)
+    tree.should.have.nested.property('electrical.switch.0.3.state.value', true)
+    tree.should.have.nested.property('electrical.switch.0.4.state.value', false)
+    tree.should.have.nested.property('electrical.switch.0.5.state.value', false)
+    tree.should.have.nested.property('electrical.switch.0.6.state.value', false)
+    tree.should.have.nested.property('electrical.switch.0.7.state.value', false)
+    // tree.should.be.validSignalKVesselIgnoringIdentity
+  })
+})
