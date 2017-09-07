590f640d9cb216310543617880b8cde811c34eeb
Ludovic Francois <ludo@msolution.io>
2017-09-06 17:33:36 -0700

    moved to lowrance proprietary folder

--- pgns/65285.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {
-    source: 'Actual Temperature',
-    node: 'environment.engine_room.temperature',
-    filter: function (n2k) {
-      return n2k.fields['Actual Temperature']
-    }
-  }
-]
