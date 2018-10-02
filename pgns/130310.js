9e28e42ed7dbb9988ecc08c785b5066f02db9149
free-x <oroitburd@gmail.com>
2018-10-02 17:38:38 +0200

    Extend pgn 130310 for water temperature (#130)

--- pgns/130310.js
+++ pgns/130310.js
@@ -14,5 +14,14 @@ module.exports = [
     value: function (n2k) {
       return Number(n2k.fields['Atmospheric Pressure'])
     }
+  },
+  {
+     node: 'environment.water.temperature',
+     filter: function (n2k) {
+       return n2k.fields['Water Temperature']
+     },
+     value: function (n2k) {
+	return Number(n2k.fields['Water Temperature'])
+     }
   }
 ]
