f802d6252ad374afb42e675e71fe471cf8f86017
Claude Sammut <c.sammut@codingcrew.com.mt>
2019-01-17 20:56:04 +0100

    Added trim support

--- pgns/127488.js
+++ pgns/127488.js
@@ -26,5 +26,41 @@ module.exports = [
       var rpm = Number(chooseField(n2k, 'Engine Speed', 'Speed'))
       return rpm / 60.0
     }
+  },
+  {
+    node: 'propulsion.port.trimPosition',
+    filter: function (n2k) {
+      return (
+        chooseField(n2k, 'Engine Instance', 'Instance') ===
+        'Single Engine or Dual Engine Port'
+      )
+    },
+    value: function (n2k) {
+      var trimPos = Number(n2k.fields['Tilt/Trim'])
+
+      if(trimPos > 0){
+        trimPos = trimPos/100
+      }
+
+      return trimPos
+    }
+  },
+  {
+    node: 'propulsion.starboard.trimPosition',
+    filter: function (n2k) {
+      return (
+        chooseField(n2k, 'Engine Instance', ['Instance']) ===
+        'Dual Engine Starboard'
+      )
+    },
+    value: function (n2k) {
+      var trimPos = Number(n2k.fields['Tilt/Trim'])
+
+      if(trimPos > 0){
+        trimPos = trimPos/100
+      }
+
+      return trimPos
+    }
   }
 ]
