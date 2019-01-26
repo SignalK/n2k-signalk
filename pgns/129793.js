267a689108e82d1d0beea1d8b212f1377fce1c62
Teppo Kurki <teppo.kurki@iki.fi>
2019-01-26 22:59:10 +0200

    feature: add pgn 129793 AIS base station

---
+++ pgns/129793.js
@@ -0,0 +1,34 @@
+const getMmsiContext = require('../mmsi-context')
+const getFromStarboard = require('../aisFromStarboard')
+const getShipType = require('../aisShipTypeMapping')
+
+module.exports = [
+  {
+    value: function (n2k) {
+      return {
+        longitude: Number(n2k.fields.Longitude),
+        latitude: Number(n2k.fields.Latitude)
+      }
+    },
+    filter: function (n2k) {
+      return (
+        typeof n2k.fields['Longitude'] !== 'undefined' &&
+        typeof n2k.fields['Latitude'] !== 'undefined'
+      )
+    },
+    node: 'navigation.position'
+  },
+  {
+    node: '',
+    value: function (n2k) {
+      return {
+        mmsi: n2k.fields['User ID'].toString()
+      }
+    }
+  },
+  {
+    context: function (n2k) {
+        return 'shore.basestations.urn:mrn:imo:mmsi:' + n2k.fields['User ID']
+      }
+    }
+]
