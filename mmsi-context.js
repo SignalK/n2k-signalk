a8a80189672065ba07a68149a5b796d4e6813e81
Scott Bender <scott@scottbender.net>
2019-04-14 12:24:23 -0400

    feature: recognize AIS messages about `self`

--- mmsi-context.js
+++ mmsi-context.js
@@ -1,3 +1,12 @@
+
+const transceiverInformation = 'AIS Transceiver information'
+
 module.exports = function (n2k) {
+  if ( transceiverInformation in n2k.fields ) {
+    const val = n2k.fields[transceiverInformation]
+    if ( val && val === 'Channel A VDL transmission' || val === 'Channel B VDL transmission' || val === 'Own information not broadcast' ) {
+      return undefined
+    }
+  }
   return 'vessels.urn:mrn:imo:mmsi:' + n2k.fields['User ID']
 }
