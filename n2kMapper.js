7aeb21f129624a4d68f7d13a7a55521554014c21
Scott Bender <scott@scottbender.net>
2019-06-01 11:20:05 -0400

    feature: provide meta data for devices on the network

--- n2kMapper.js
+++ n2kMapper.js
@@ -1,11 +1,45 @@
+const EventEmitter = require('events').EventEmitter
 var through = require('through')
 var debug = require('debug')('signalk:n2k-signalk')
+
+require('util').inherits(N2kMapper, EventEmitter);
 
 var n2kMappings = {}
 Object.assign(n2kMappings, require('./pgns'))
 Object.assign(n2kMappings, require('./fusion'))
 Object.assign(n2kMappings, require('./lowrance'))
 Object.assign(n2kMappings, require('./raymarine'))
+
+function N2kMapper (options) {
+  this.state = {}
+
+  this.on('n2kRequestMetadata', (src) => {
+    this.emit('n2kOut', {
+      "pgn": 59904,
+      "dst": src,
+      "PGN": 126996
+    })
+    this.emit('n2kOut', {
+      "pgn": 59904,
+      "dst": src,
+      "PGN": 126998
+    })
+    this.emit('n2kOut', {
+      "pgn": 59904,
+      "dst": src,
+      "PGN": 60928
+    })
+  })
+}
+
+N2kMapper.prototype.toDelta = function(n2k) {
+  if ( metaPGNs[n2k.pgn] ) {
+    const meta = metaPGNs[n2k.pgn](n2k)
+    this.emit('n2kSourceMetadata', n2k, meta)
+  } else {
+    return toDelta(n2k, this.state)
+  }
+}
 
 var toDelta = function (n2k, state) {
   try {
@@ -185,6 +219,41 @@ function addAsNested (pathValue, source, timestamp, result) {
   }
 }
 
+const metaPGNs = {
+  60928: (n2k) => {
+    return {
+      uniqueId: n2k.fields['Unique Number'],
+      manufacturerName: n2k.fields['Manufacturer Code'],
+      deviceFunction: n2k.fields['Device Function'],
+      deviceClass: n2k.fields['Device Class'],
+      deviceInstanceLower: n2k.fields['Device Instance Lower'],
+      deviceInstanceUpper: n2k.fields['Device Instance Upper'],
+      systemInstance: n2k.fields['System Instance']
+    }
+  },
+  126998: (n2k) => {
+    return {
+      installationNote1: n2k.fields['Installation Description #1'],
+      installationNote2: n2k.fields['Installation Description #2'],
+      installationNote3: n2k.fields['Installation Description #3'],
+      manufacturerInfo: n2k.fields['Manufacturer Information']
+    }
+  },
+  126996: (n2k) => {
+    return {
+      productName: n2k.fields['Model ID'],
+      hardwareVersion: n2k.fields['Model Version'],
+      softwareVersion: n2k.fields['Software Version Code'],
+      productID: n2k.fields['Product Code'],
+      serialNumber: n2k.fields['Model Serial Code'],
+      nmea2000Version: n2k.fields['NMEA 2000 Version'],
+      certificationLevel: n2k.fields['Certification Level'],
+      loadEquivalency: n2k.fields['Load Equivalency']
+    }
+  }
+}
+
+exports.N2kMapper = N2kMapper
 exports.toDelta = toDelta
 exports.toDeltaTransformer = function (options) {
   return through(function (data) {
