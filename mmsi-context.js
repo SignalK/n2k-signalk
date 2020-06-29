936d71f7a00491bbed11c6298892c79be1fc8908
Scott Bender <scott@scottbender.net>
2020-06-29 13:30:21 -0400

    fix: use of AIS Transceiver information to detect self messages is not reliable (#186)

--- mmsi-context.js
+++ mmsi-context.js
@@ -1,12 +1,4 @@
 
-const transceiverInformation = 'AIS Transceiver information'
-
 module.exports = function (n2k) {
-  if ( transceiverInformation in n2k.fields ) {
-    const val = n2k.fields[transceiverInformation]
-    if ( val && val === 'Channel A VDL transmission' || val === 'Channel B VDL transmission' || val === 'Own information not broadcast' ) {
-      return undefined
-    }
-  }
   return n2k.fields['User ID'] ? 'vessels.urn:mrn:imo:mmsi:' + n2k.fields['User ID'] : undefined
 }
