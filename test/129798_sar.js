8418f39d10b71746d90997ded0527d69e0dc0425
Teppo Kurki <teppo.kurki@iki.fi>
2021-12-30 09:35:13 +0200

    feature: sensors.ais.class @ 129798

--- test/129798_sar.js
+++ test/129798_sar.js
@@ -1,4 +1,5 @@
 var chai = require('chai')
+const { assertSensorClass } = require('./ais_utils')
 chai.Should()
 chai.use(require('chai-things'))
 chai.use(require('@signalk/signalk-schema').chaiModule)
@@ -13,6 +14,7 @@ describe('129798 AIS SAR Aircraft Position Report', function () {
     var delta = mapper.toDelta(msg)
     delta.updates.length.should.equal(1)
     delta.context.should.equal('sar.urn:mrn:imo:mmsi:100046')
+    assertSensorClass(delta, 'SAR')
     var tree = mapper.toNested(msg)
     tree.navigation.position.value.longitude.should.equal(-75.8338099)
     tree.navigation.position.value.latitude.should.equal(39.6475617)
@@ -21,7 +23,10 @@ describe('129798 AIS SAR Aircraft Position Report', function () {
       2.227
     )
     tree.should.have.nested.property('navigation.speedOverGround.value', 694.4)
-    console.log(JSON.stringify(tree))
+
+    //TODO remove when sensors.ais.class is in schema
+    delete tree.sensors.ais
+
     tree.should.be.validSignalKVesselIgnoringIdentity
   })
 })
