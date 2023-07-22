f182e40f398e176ca7d798d605c267417bc71020
Scott Bender <scott@scottbender.net>
2023-07-22 13:14:29 -0400

    fix: null time values not handled properly

--- utils.js
+++ utils.js
@@ -44,7 +44,7 @@ function acPhase (n2k) {
 }
 
 function timeToSeconds (time) {
-  if (typeof time !== 'undefined') {
+  if (typeof time !== 'undefined' && typeof time !== null) {
     let split = time.split(':')
     if (split.length === 3) {
       return Number(split[0]) * 3600 + Number(split[1]) * 60 + Number(split[2])
