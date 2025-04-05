14eebdfc56ac4d792e2b6c5b7a1772a6a6187ab8
Scott Bender <scott@scottbender.net>
2025-04-05 15:22:18 -0400

    fix: an error is thrown when when a devices src id changes (#284)
    n2k-signalk: address 0 changed from c0328700227b5a77 c0509b002270da77
    TypeError: Cannot set properties of undefined (setting '60928')
        at N2kMapper.toDelta
            (/usr/local/lib/node_modules/signalk-server/node_modules/@signalk/n2k-signalk/n2kMapper.js:140:51)

--- n2kMapper.js
+++ n2kMapper.js
@@ -105,11 +105,9 @@ N2kMapper.prototype.toDelta = function (n2k) {
   if (metaPGNs[n2k.pgn]) {
     const meta = metaPGNs[n2k.pgn](n2k)
     if (!this.state[n2k.src]) {
-      this.state[n2k.src] = {}
-    }
-
-    if (!this.state[n2k.src].metaPGNsReceived) {
-      this.state[n2k.src].metaPGNsReceived = {}
+      this.state[n2k.src] = {
+        metaPGNsReceived: {}
+      }
     }
 
     if (n2k.pgn === 60928) {
@@ -127,7 +125,9 @@ N2kMapper.prototype.toDelta = function (n2k) {
           this.state[n2k.src].canName,
           canName
         )
-        this.state[n2k.src] = {}
+        this.state[n2k.src] = {
+          metaPGNsReceived: {}
+        }
         this.requestMetaData(n2k.src, 126996).then(() => {
           return this.requestMetaData(n2k.src, 126998)
         })
