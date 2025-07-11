b8a2d107dcbc6cd70e18f6745f213142d3e80164
Scott Bender <scott@scottbender.net>
2025-07-11 13:50:40 -0400

    fix: bin scripts not working

--- bin/demo.js
+++ bin/demo.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var n2kMapper = require('../n2kMapper.js');
+var n2kMapper = require('../dist/n2kMapper.js');
 var msg = {
   "timestamp": "2013-10-08T15:47:28.263Z",
   "prio": "2",
@@ -9,7 +9,7 @@ var msg = {
   "pgn": "127251",
   "description": "Rate of Turn",
   "fields": {
-    "Rate": "0.0066071"
+    "rate": "0.0066071"
   }
 };
 
