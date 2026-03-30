24c37e7f9b792dabacf2663f29f6fac9669d3e35
Fabian Fisahn <fisahn@its-immaterial.com>
2026-03-30 19:44:22 +0200

    add: Course API-aligned paths for route data

--- pgns/129285.js
+++ pgns/129285.js
@@ -3,27 +3,127 @@ const _ = require('lodash')
 
 module.exports = [
   {
-    node: 'navigation.currentRoute.name',
+    node: function (n2k, state) {
+      return (
+        'navigation.course' +
+        state.lastCourseCalculationType +
+        '.activeRoute.name'
+      )
+    },
+    filter: function (n2k, state) {
+      return (
+        typeof state === 'object' &&
+        typeof state.lastCourseCalculationType !== 'undefined'
+      )
+    },
     source: 'routeName'
   },
   {
-    node: 'navigation.currentRoute.waypoints',
-    filter: n2k => {
-      return !_.isUndefined(n2k.fields.list)
+    node: function (n2k, state) {
+      return (
+        'navigation.course' +
+        state.lastCourseCalculationType +
+        '.previousPoint.name'
+      )
     },
-    value: n2k => {
-      var idx = 0
-      return n2k.fields.list.map(wp => {
-        return {
-          name: wp.wpName,
-          position: {
-            value: {
-              latitude: wp.wpLatitude,
-              longitude: wp.wpLongitude
-            }
-          }
-        }
-      })
+    filter: function (n2k, state) {
+      return (
+        typeof state === 'object' &&
+        typeof state.lastCourseCalculationType !== 'undefined' &&
+        !_.isUndefined(n2k.fields.list) &&
+        n2k.fields.list.length > 0
+      )
+    },
+    value: function (n2k) {
+      return n2k.fields.list[0].wpName
+    }
+  },
+  {
+    node: function (n2k, state) {
+      return (
+        'navigation.course' +
+        state.lastCourseCalculationType +
+        '.previousPoint.position'
+      )
+    },
+    filter: function (n2k, state) {
+      return (
+        typeof state === 'object' &&
+        typeof state.lastCourseCalculationType !== 'undefined' &&
+        !_.isUndefined(n2k.fields.list) &&
+        n2k.fields.list.length > 0
+      )
+    },
+    value: function (n2k) {
+      return {
+        latitude: n2k.fields.list[0].wpLatitude,
+        longitude: n2k.fields.list[0].wpLongitude
+      }
+    }
+  },
+  {
+    node: function (n2k, state) {
+      return (
+        'navigation.course' +
+        state.lastCourseCalculationType +
+        '.nextPoint.name'
+      )
+    },
+    filter: function (n2k, state) {
+      return (
+        typeof state === 'object' &&
+        typeof state.lastCourseCalculationType !== 'undefined' &&
+        !_.isUndefined(n2k.fields.list) &&
+        n2k.fields.list.length > 1
+      )
+    },
+    value: function (n2k) {
+      return n2k.fields.list[1].wpName
+    }
+  },
+  {
+    node: function (n2k, state) {
+      return (
+        'navigation.course' +
+        state.lastCourseCalculationType +
+        '.followingPoint.name'
+      )
+    },
+    filter: function (n2k, state) {
+      return (
+        typeof state === 'object' &&
+        typeof state.lastCourseCalculationType !== 'undefined' &&
+        !_.isUndefined(n2k.fields.list) &&
+        n2k.fields.list.length > 2 &&
+        !_.isUndefined(n2k.fields.list[2].wpLatitude) &&
+        !_.isUndefined(n2k.fields.list[2].wpLongitude)
+      )
+    },
+    value: function (n2k) {
+      return n2k.fields.list[2].wpName
+    }
+  },
+  {
+    node: function (n2k, state) {
+      return (
+        'navigation.course' +
+        state.lastCourseCalculationType +
+        '.followingPoint.position'
+      )
+    },
+    filter: function (n2k, state) {
+      return (
+        typeof state === 'object' &&
+        typeof state.lastCourseCalculationType !== 'undefined' &&
+        !_.isUndefined(n2k.fields.list) &&
+        n2k.fields.list.length > 2
+      )
+    },
+    value: function (n2k) {
+      return {
+        latitude: n2k.fields.list[2].wpLatitude,
+        longitude: n2k.fields.list[2].wpLongitude
+      }
     }
   }
 ]
